f70e2d281832214016c48d57bc17cb78efff0e82
Weibin <weibinchann@gmail.com>
2026-08-20 18:32:43 +0800

    Rename chatbot to Nova
    Change all occurences of previous name Duke to
    Nova within the repo

---
+++ src/main/java/Nova.java
@@ -0,0 +1,15 @@
+/**
+ * Entry point for the Nova chatbot.
+ * At present it only prints the startup banner.
+ */
+public class Nova {
+    public static void main(String[] args) {
+        String banner = " _   _                      \n"
+                + "| \\ | |  ___           __ _ \n"
+                + "|  \\| | / _ \\ __   __ / _` |\n"
+                + "| |\\  || (_) |\\ \\ / /| (_| |\n"
+                + "|_| \\_| \\___/  \\ V /  \\__,_|\n"
+                + "                \\_/         \n";
+        System.out.println(banner);
+    }
+}
