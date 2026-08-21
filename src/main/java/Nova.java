1cf9e7511ce2104900f2cb371e31a890006b54e5
Weibin <weibinchann@gmail.com>
2026-08-21 12:36:15 +0800

    Refactor greeting and farewell
    Used AI feedback to abstract String values into constants
    Separated greeting and farewell into individual functions

--- src/main/java/Nova.java
+++ src/main/java/Nova.java
@@ -3,19 +3,31 @@
  * At present it only prints the startup banner.
  */
 public class Nova {
+    private static final String NAME = "Nova";
+    private static final String BANNER = " _   _                      \n"
+            + "| \\ | |  ___           __ _ \n"
+            + "|  \\| | / _ \\ __   __ / _` |\n"
+            + "| |\\  || (_) |\\ \\ / /| (_| |\n"
+            + "|_| \\_| \\___/  \\ V /  \\__,_|\n"
+            + "                \\_/         ";
+    private static final String DIVIDER = "_".repeat(60);
+    private static final String HELLO_MESSAGE = String.format("Hello! I'm %s.\nWhat can I do for you?", NAME);
+    private static final String BYE_MESSAGE = "Bye. Hope to see you again soon!";
+
     public static void main(String[] args) {
-        String banner = " _   _                      \n"
-                + "| \\ | |  ___           __ _ \n"
-                + "|  \\| | / _ \\ __   __ / _` |\n"
-                + "| |\\  || (_) |\\ \\ / /| (_| |\n"
-                + "|_| \\_| \\___/  \\ V /  \\__,_|\n"
-                + "                \\_/         ";
+        printGreeting();
+        printFarewell();
+    }
 
-        String horizontalLine = "____________________________________________________________\n";
-        String helloMessage = "Hello! I'm Nova.\nWhat can I do for you?";
-        String byeMessage = "Bye. Hope to see you again soon!";
+    private static void printGreeting() {
+        System.out.println(DIVIDER);
+        System.out.println(BANNER);
+        System.out.println(HELLO_MESSAGE);
+        System.out.println(DIVIDER);
+    }
 
-        System.out.println(horizontalLine + banner + "\n" + helloMessage + "\n" +
-                horizontalLine + byeMessage + "\n" + horizontalLine);
+    private static void printFarewell() {
+        System.out.println(BYE_MESSAGE);
+        System.out.println(DIVIDER);
     }
 }
