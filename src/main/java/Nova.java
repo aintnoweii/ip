2df157918b12ca9369764ef82aa5840315e941d3
Weibin <weibinchann@gmail.com>
2026-08-21 11:58:05 +0800

    Add greeting

--- src/main/java/Nova.java
+++ src/main/java/Nova.java
@@ -9,7 +9,13 @@ public class Nova {
                 + "|  \\| | / _ \\ __   __ / _` |\n"
                 + "| |\\  || (_) |\\ \\ / /| (_| |\n"
                 + "|_| \\_| \\___/  \\ V /  \\__,_|\n"
-                + "                \\_/         \n";
-        System.out.println(banner);
+                + "                \\_/         ";
+
+        String horizontalLine = "____________________________________________________________\n";
+        String helloMessage = "Hello! I'm Nova.\nWhat can I do for you?";
+        String byeMessage = "Bye. Hope to see you again soon!";
+
+        System.out.println(horizontalLine + banner + "\n" + helloMessage + "\n" +
+                horizontalLine + byeMessage + "\n" + horizontalLine);
     }
 }
